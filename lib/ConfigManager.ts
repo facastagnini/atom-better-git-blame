cd81bdac7a01b55a6b171a70c9030beacd1cd8d7
Nick Omeyer <nick@stepsize.com>
2017-11-08 20:22:33 +0000

    Allow age section of blame popover to be hidden
    Relates to issue #25

--- lib/ConfigManager.ts
+++ lib/ConfigManager.ts
@@ -34,13 +34,20 @@ export const config = {
     ],
     order: 3,
   },
+  displayAgeSection: {
+    title: 'Display code age in popover',
+    description: 'When viewing the blame popover, display the section visualising the age of the code',
+    type: 'boolean',
+    default: true,
+    order: 4,
+  },
   truncateGutterNames: {
     title: 'Truncate author names in gutter',
     description:
       'Attempt to truncate commit author names to display first initial and surname only',
     type: 'boolean',
     default: true,
-    order: 4,
+    order: 5,
   },
   highlightPullRequestOnHover: {
     title: 'Highlight pull request on hover',
@@ -48,7 +55,7 @@ export const config = {
       'When viewing the blame popover, highlight lines introduced by commits from the same pull request',
     type: 'boolean',
     default: true,
-    order: 5,
+    order: 6,
   },
   displayHighlightLabels: {
     title: 'Display highlight labels',
@@ -56,14 +63,14 @@ export const config = {
       'When viewing the blame popover, show commit hashes and pull requests numbers in the top right of highlighted sections',
     type: 'boolean',
     default: true,
-    order: 6,
+    order: 7,
   },
   sendUsageStatistics: {
     title: 'Send anonymous usage statistics',
     description: 'Send anonymous usage data to Stepsize so we can improve the plugin',
     type: 'boolean',
     default: true,
-    order: 7,
+    order: 8,
   },
   parallelGitProcessing: {
     title: 'Use parallel processing for Git commands',
@@ -71,7 +78,7 @@ export const config = {
       'Can improve performance on multi-core machines, if the gutter is slow try disabling this',
     type: 'boolean',
     default: true,
-    order: 8,
+    order: 9,
   },
   searchInLayerEnabled: {
     title: 'Enable Search in Layer (macOS only)',
@@ -79,7 +86,7 @@ export const config = {
       'Send code selection events via UDP to the Layer desktop app to use its search functionality',
     type: 'boolean',
     default: true,
-    order: 9,
+    order: 10,
   },
 };
 
